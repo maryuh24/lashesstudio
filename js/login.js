bb191ff712bead806417328bbd4d9b23335dc784
maryuh24 <masofiamagdadaro@gmail.com>
2025-12-17 16:58:17 +0800

    Update login API endpoint to production URL

--- js/login.js
+++ js/login.js
@@ -99,7 +99,7 @@ $(document).ready(function() {
       showLoginLoading();
 
       try {
-        const res = await fetch("http://127.0.0.1:8000/api/auth/login", {
+        const res = await fetch("https://lashstudio.ccs4thyear.com/api/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data)
